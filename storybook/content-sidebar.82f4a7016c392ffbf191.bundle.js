b7a1404410cc30fd2673bdd0a1f757bc0896db82
Jenkins CI <jenkins@box.com>
2022-06-23 18:42:12 -0700

    build(examples): v15.1.0-beta.18

---
+++ storybook/content-sidebar.82f4a7016c392ffbf191.bundle.js
@@ -0,0 +1,3 @@
+/*! For license information please see content-sidebar.82f4a7016c392ffbf191.bundle.js.LICENSE */
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{4349:function(module,__webpack_exports__,__webpack_require__){"use strict";__webpack_require__.r(__webpack_exports__);var _ContentSidebar__WEBPACK_IMPORTED_MODULE_0__=__webpack_require__(1393);__webpack_require__.d(__webpack_exports__,"default",(function(){return _ContentSidebar__WEBPACK_IMPORTED_MODULE_0__.a}))}}]);
+//# sourceMappingURL=content-sidebar.82f4a7016c392ffbf191.bundle.js.map
